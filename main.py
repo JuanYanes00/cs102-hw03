0523d27c3cd0c724d6f8ea5cbf3bb0a4af1b94a1
Juan Yanes <juan.yanesbenatuil@cooper.edu>
2019-11-28 10:31:01 -0400

    Juan Yanes Homework

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
-
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
 
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
-
-    # TODO: Fill in the actual logic here!
-
+    with open(input_file_path, "r") as f:
+        for line in f:
+            splittedlines = line.split(",")
+            empty = []
+            for x in splittedlines:
+                empty.append(float(x))
+            print(f"{mean(empty)}")
 
 if __name__ == "__main__":
     main()
